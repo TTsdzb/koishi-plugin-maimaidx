c21a4883bae82e4a62d1afb14e976541089c2f30
TTsdzb <ttsdzb@outlook.com>
2023-10-20 17:29:24 +0800

    feat: plugin config and dependent service

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
-import { Context, Schema } from 'koishi'
+import { Context, Logger, Schema } from "koishi";
 
-export const name = 'maimaidx'
+export const name = "maimaidx";
+export const using = ["database", "puppeteer"];
 
-export interface Config {}
+export interface Config {
+  assetsPath: string;
+}
 
-export const Config: Schema<Config> = Schema.object({})
+export const Config: Schema<Config> = Schema.object({
+  assetsPath: Schema.path().required().description("资源文件的存储路径"),
+});
 
-export function apply(ctx: Context) {
-  // write your plugin here
+export function apply(ctx: Context, config: Config) {
+  const logger = new Logger("maimaidx");
+  logger.info(config);
 }
